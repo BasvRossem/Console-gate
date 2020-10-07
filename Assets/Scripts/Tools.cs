3d7bd8cd70da731896bead32807fa83811ca2a62
Bas van Rossem <bas.v.rossem@gmail.com>
2020-10-07 16:54:27 +0200

    Made a tool class

---
+++ Assets/Scripts/Tools.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tools
+{
+    public Tools()
+    {
+    }
+
+    public static bool CheckError(bool condition, string errorMessage)
+    {
+        if (condition)
+        {
+            Debug.LogError(errorMessage);
+        }
+        return condition;
+    }
+}
