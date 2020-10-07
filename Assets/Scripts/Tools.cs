81260325d11f76e577225a957e0f30178dfce41a
Bas van Rossem <bas.v.rossem@gmail.com>
2020-10-07 17:33:13 +0200

    Added documentation

--- Assets/Scripts/Tools.cs
+++ Assets/Scripts/Tools.cs
@@ -8,6 +8,12 @@ public class Tools
     {
     }
 
+    /// <summary>
+    /// Check the condition and throw an error with a custom message if the condition is true.
+    /// </summary>
+    /// <param name="condition">Condition to check.</param>
+    /// <param name="errorMessage">Message to throw on error.</param>
+    /// <returns></returns>
     public static bool CheckError(bool condition, string errorMessage)
     {
         if (condition)
