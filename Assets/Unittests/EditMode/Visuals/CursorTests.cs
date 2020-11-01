363298778cb54040c6e83d32079bd55a675d7ac8
Bas van Rossem <bas.v.rossem@gmail.com>
2020-11-01 21:26:09 +0100

    Added tests for all visual (non monobehaviour) classes

---
+++ Assets/Unittests/EditMode/Visuals/CursorTests.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using Cursor = Visuals.Cursor;
+
+namespace VisualsTests
+{
+    public class CursorTests
+    {
+        private Cursor _cursor;
+
+        [SetUp]
+        public void Setup()
+        {
+            _cursor = new Cursor();
+        }
+
+        [Test]
+        public void CheckDirections()
+        {
+            Assert.AreEqual(new Vector2Int(-1, 0), Cursor.Left);
+            Assert.AreEqual(new Vector2Int(1, 0), Cursor.Right);
+            Assert.AreEqual(new Vector2Int(0, -1), Cursor.Up);
+            Assert.AreEqual(new Vector2Int(0, 1), Cursor.Down);
+        }
+
+        [Test]
+        public void SetBoundsGetBounds()
+        {
+            _cursor.SetBounds(5, 6, 10, 11);
+
+            Assert.AreEqual(new List<Vector2Int>() { new Vector2Int(5, 6), new Vector2Int(10, 11) }, _cursor.GetBounds());
+
+            _cursor.SetPosition(5, 5);
+            _cursor.SetBounds(10, 10, 15, 15);
+            Assert.AreEqual(new Vector2Int(10, 10), _cursor.GetPosition());
+
+            _cursor.SetPosition(12, 12);
+            Assert.AreEqual(new Vector2Int(12, 12), _cursor.GetPosition());
+        }
+
+        [Test]
+        public void SetPositionGetPosition()
+        {
+            _cursor.SetPosition(5, 5);
+            Assert.AreEqual(new Vector2Int(5, 5), _cursor.GetPosition());
+
+            _cursor.SetPosition(10, 10);
+            Assert.AreEqual(new Vector2Int(10, 10), _cursor.GetPosition());
+        }
+
+        [Test]
+        public void Move()
+        {
+            _cursor.Move(Cursor.Right);
+            Assert.AreEqual(new Vector2Int(1, 0), _cursor.GetPosition());
+            _cursor.Move(Cursor.Left);
+            Assert.AreEqual(new Vector2Int(0, 0), _cursor.GetPosition());
+            _cursor.Move(Cursor.Down);
+            Assert.AreEqual(new Vector2Int(0, 1), _cursor.GetPosition());
+            _cursor.Move(Cursor.Up);
+            Assert.AreEqual(new Vector2Int(0, 0), _cursor.GetPosition());
+        }
+
+        [Test]
+        public void MoveWithinBounds()
+        {
+            _cursor.SetBounds(5, 5, 10, 10);
+            _cursor.ResetPosition();
+            Assert.AreEqual(5, _cursor.x);
+            Assert.AreEqual(5, _cursor.y);
+
+            _cursor.Move(Cursor.Left);
+            Assert.AreEqual(5, _cursor.x);
+            Assert.AreEqual(5, _cursor.y);
+
+            for (int moves = 0; moves < 50; moves++)
+            {
+                _cursor.Move(Cursor.Right);
+                _cursor.Move(Cursor.Down);
+            }
+
+            Assert.AreEqual(10, _cursor.x);
+            Assert.AreEqual(10, _cursor.y);
+        }
+
+        [Test]
+        public void ResetPosition()
+        {
+            _cursor.SetPosition(1, 10);
+            _cursor.ResetPosition();
+            Assert.AreEqual(new Vector2Int(0, 0), _cursor.GetPosition());
+        }
+    }
+}
---
+++ Assets/Unittests/EditMode/Visuals/ViewTests.cs
@@ -0,0 +1,117 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Visuals;
+
+namespace VisualsTests
+{
+    public class ViewTests
+    {
+        private View _view;
+        private GridSize _size = new GridSize(5, 5);
+        private GridPosition _start = new GridPosition(3, 3);
+        private GridPosition _monitor = new GridPosition(8, 8);
+
+        [SetUp]
+        public void Setup()
+        {
+            _view = new View(_size, _start, _monitor);
+        }
+
+        [Test]
+        public void Constructor()
+        {
+            Assert.AreEqual(_size, _view.size);
+            Assert.AreEqual(_start, _view.internalPosition);
+            Assert.AreEqual(_monitor, _view.externalPosition);
+        }
+
+        [Test]
+        public void SetSizeNegative()
+        {
+            LogAssert.Expect(LogType.Error, "Size cannot be negative or zero.");
+            var size = new GridSize(-5, -5);
+
+            _view.SetSize(size);
+        }
+
+        [Test]
+        public void SetExternalPositionNegative()
+        {
+            LogAssert.Expect(LogType.Error, "External position cannot be negative.");
+            var position = new GridPosition(-5, -5);
+
+            _view.SetExternalPosition(position);
+        }
+
+        [Test]
+        public void SetInternalPositionNegative()
+        {
+            LogAssert.Expect(LogType.Error, "Internal position cannot be negative.");
+            var position = new GridPosition(-5, -5);
+
+            _view.SetInternalPosition(position);
+        }
+
+        [Test]
+        public void HasChangedAfterInit()
+        {
+            Assert.IsTrue(_view.HasChanged());
+        }
+
+        [Test]
+        public void SetTextGrid()
+        {
+            var grid = new TextGrid(new GridSize(24, 80));
+            _view.SetSize(new GridSize(24, 80));
+            _view.SetInternalPosition(new GridPosition(0));
+            _view.SetText(grid);
+            Assert.AreEqual(grid.GetSize(), _view.textGrid.GetSize());
+        }
+
+        [Test]
+        public void MoveInternalPositionPositive()
+        {
+            _view.SetInternalPosition(new GridPosition());
+
+            GridPosition positionBefore = _view.internalPosition;
+            _view.MoveInternalPosition(1, 1, 1, 1);
+            GridPosition positionAfter = _view.internalPosition;
+            Assert.AreEqual(positionBefore, positionAfter);
+
+            _view.MoveInternalPosition(down: 1, right: 1);
+            var expected = new GridPosition(1, 1);
+            GridPosition actual = _view.internalPosition;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void MoveInternalPositionNegative()
+        {
+            LogAssert.Expect(LogType.Error, "Cannot move in a negative direction.");
+            
+            _view.internalPosition = new GridPosition();
+            _view.MoveInternalPosition(-1, 0, 1, 5);
+        }
+        
+        [Test]
+        public void MoveInternalPositionTooMuchLeftAndUp()
+        {
+            LogAssert.Expect(LogType.Warning, "New view internal position row cannot be negative. Set to 0.");
+            LogAssert.Expect(LogType.Warning, "New view internal position column cannot be negative. Set to 0.");
+            _view.internalPosition = new GridPosition();
+            _view.MoveInternalPosition(1, 0, 2, 1);
+            Assert.AreEqual(new GridPosition(), _view.internalPosition);
+        }
+
+        [Test]
+        public void GrowInAllDirections()
+        {
+            _view.SetInternalPosition(new GridPosition(5,5));
+            _view.SetSize(new GridSize(10, 10));
+            _view.Grow(1,1,1,1);
+            Assert.AreEqual(new GridPosition(4,4), _view.internalPosition);
+            Assert.AreEqual(new GridSize(12,12), _view.size);
+        }
+    }
+}
