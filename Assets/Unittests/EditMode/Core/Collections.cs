ef3d892612a674e5c1faf6f233b45ea02780166e
Bas van Rossem <bas.v.rossem@gmail.com>
2020-11-01 21:24:15 +0100

    Added core tests

---
+++ Assets/Unittests/EditMode/Core/Collections.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Collection
+    {
+        [Test]
+        public void GridSizeRows()
+        {
+            var size = new GridSize(5, 10);
+            Assert.AreEqual(5, size.rows);
+        }
+        
+        [Test]
+        public void GridSizeColumns()
+        {
+            var size = new GridSize(5, 10);
+            Assert.AreEqual(10, size.columns);
+        }
+
+        [Test]
+        public void GridSizeToString()
+        {
+            var size = new GridSize(5, 10);
+            
+            var expected = "5, 10";
+            var actual = size.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test]
+        public void GridPositionRows()
+        {
+            var size = new GridPosition(5, 10);
+            Assert.AreEqual(5, size.row);
+        }
+        
+        [Test]
+        public void GridPositionColumns()
+        {
+            var size = new GridPosition(5, 10);
+            Assert.AreEqual(10, size.column);
+        }
+
+        [Test]
+        public void GridPositionToString()
+        {
+            var size = new GridPosition(5, 10);
+            
+            var expected = "5, 10";
+            var actual = size.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
