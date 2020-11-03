a702a06295aacfa12bd98a52e9dad01552f13cdd
Bas van Rossem <bas.v.rossem@gmail.com>
2020-11-03 13:45:35 +0100

    Added drawing of lines

--- Assets/Scripts/Levels/Level2.cs
+++ Assets/Scripts/Levels/Level2.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using ControllerStructures;
+using JetBrains.Annotations;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UserInput;
@@ -9,16 +12,22 @@ using Visuals;
 
 public class LogicGates
 {
-    public static void ConnectComponents(LogicComponent input, LogicGate output, int outputPort)
+    public static void ConnectComponents(LogicComponent input, LogicGate output, int outputPort, Layer puzzleLayer, List<GridPosition> positionsHorizontal, List<GridPosition> positionsVertical)
     {
-        input.lines.Add(new Line(output, outputPort));
+        var line = new Line(output, outputPort);
+        line.AddGridPositions(puzzleLayer, positionsHorizontal, positionsVertical);
+        input.lines.Add(line);
     }
-    
+
     public class Line
     {
         public bool isActive;
         public LogicGate output;
         public int outputPort;
+
+        private Layer _layer;
+        private List<GridPosition> _characterPositionsHorizontal;
+        private List<GridPosition> _characterPositionsVertical;
 
         public Line(LogicGate output, int outputPort)
         {
@@ -26,35 +35,77 @@ public class LogicGates
             this.outputPort = outputPort;
         }
 
+        public void AddGridPositions(Layer puzzleLayer, List<GridPosition> positionsHorizontal, List<GridPosition> positionsVertical)
+        {
+            _layer = puzzleLayer;
+            _characterPositionsHorizontal = positionsHorizontal;
+            _characterPositionsVertical = positionsVertical;
+        }
+
         public void SetActive(bool value)
         {
             isActive = value;
             output.inputs[outputPort] = isActive;
             output.CalculateOutput();
+
+            UpdateCharacters();
+        }
+
+        private void UpdateCharacters()
+        {
+            UpdateCharactersHorizontal();
+            UpdateCharactersVertical();
+            _layer.Change(true);
+        }
+
+        private void UpdateCharactersVertical()
+        {
+            var character = '.';
+            if (_characterPositionsVertical.Count == 0) return;
+            if (isActive) character = '|';
+            
+            foreach (GridPosition position in _characterPositionsVertical)
+            {
+                _layer.textGrid[position.row, position.column] = character;
+            }
+        }
+
+        private void UpdateCharactersHorizontal()
+        {
+            var character = '.';
+            if (_characterPositionsHorizontal.Count == 0) return;
+            if (isActive) character = '_';
+            
+            foreach (GridPosition position in _characterPositionsHorizontal)
+            {
+                _layer.textGrid[position.row, position.column] = character;
+            }
         }
     }
-    
+
     public class LogicComponent
     {
         public List<Line> lines;
-        
+
         protected LogicComponent()
         {
             lines = new List<Line>();
         }
     }
-    
-    public class LogicGate: LogicComponent
+
+    public class LogicGate : LogicComponent
     {
         public bool[] inputs = {false, false};
-        
-        public LogicGate(){}
+
+        public LogicGate()
+        {
+        }
 
         protected virtual bool IsActivated()
         {
             return false;
         }
-        
+
         public virtual void CalculateOutput()
         {
             foreach (var line in lines)
@@ -63,16 +114,20 @@ public class LogicGates
             }
         }
     }
-    
-    public class Switch: LogicComponent
-    {
-        private bool _activated = false;
 
-        public Switch(){}
+    public class Switch : LogicComponent
+    {
+        private bool _activated;
+
+        public Switch()
+        {
+            _activated = false;
+        }
 
         public void Toggle()
         {
             _activated = !_activated;
+            Debug.Log($"switch is now {_activated}");
             foreach (var line in lines)
             {
                 line.SetActive(_activated);
@@ -82,36 +137,42 @@ public class LogicGates
 
     public class AndGate : LogicGate
     {
-        public AndGate(){}
-        
+        public AndGate()
+        {
+        }
+
         protected override bool IsActivated()
         {
-            return inputs[0]&& inputs[1];
+            return inputs[0] && inputs[1];
         }
     }
-    
+
     public class OrGate : LogicGate
     {
-        public OrGate(){}
-        
+        public OrGate()
+        {
+        }
+
         protected override bool IsActivated()
         {
             return inputs[0] || inputs[1];
         }
     }
-    
+
     public class LoginGate : LogicGate
     {
-        public LoginGate(){}
-        
+        public LoginGate()
+        {
+        }
+
         protected override bool IsActivated()
         {
             return inputs[0] || inputs[1];
         }
-        
+
         public override void CalculateOutput()
         {
-            if(IsActivated()) Debug.Log("Solved!");
+            if (IsActivated()) Debug.Log("Solved!");
         }
     }
 }
@@ -122,30 +183,41 @@ public class Level2 : MonoBehaviour
     [SerializeField] private KeyListener keyListener = null;
 
     private Layer _puzzleLayer;
-    
+
     private int _textIndex = 0;
 
     private List<LogicGates.Switch> _switches;
+    private List<GridPosition> _switchPositions;
+    private int _selectedSwitch;
 
     // Start is called before the first frame update
     private void Start()
     {
         if (Tools.CheckError(monitor == null, "No Monitor object has been added")) return;
         if (Tools.CheckError(keyListener == null, "No KeyListener object has been added")) return;
-        
-        // keyListener.AddKey(new List<KeyCode> { KeyCode.Space }, SelectSwitch);
-        // keyListener.AddKey(new List<KeyCode> { KeyCode.LeftArrow }, MoveSelectLeft);
-        // keyListener.AddKey(new List<KeyCode> { KeyCode.Space }, MoveSelectRight);
+
+        keyListener.AddKey(new List<KeyCode> {KeyCode.Space}, SelectSwitch);
+        keyListener.AddKey(new List<KeyCode> {KeyCode.LeftArrow}, MoveSelectLeft);
+        keyListener.AddKey(new List<KeyCode> {KeyCode.RightArrow}, MoveSelectRight);
 
         _puzzleLayer = monitor.NewLayer();
+        _puzzleLayer.view.SetExternalPosition(new GridPosition(5, 17));
 
-        monitor.uiCursor.Show(false);
+        monitor.uiCursor.Show(true);
         // monitor.uiCursor.linkedLayer = _continueLayer;
         // monitor.uiCursor.Blink(true);
 
         _switches = new List<LogicGates.Switch>(6);
+        _switchPositions.Add(new GridPosition(12,1));
+        _switchPositions.Add(new GridPosition(12,9));
+        _switchPositions.Add(new GridPosition(12,17));
+        _switchPositions.Add(new GridPosition(12,25));
+        _switchPositions.Add(new GridPosition(12,33));
+        _switchPositions.Add(new GridPosition(12,41));
+        _selectedSwitch = 0;
 
         CreateLogicLayout();
+        LoadLogicPuzzleToLayer();
     }
 
     private void CreateLogicLayout()
@@ -157,39 +229,71 @@ public class Level2 : MonoBehaviour
         var or21 = new LogicGates.OrGate();
         var and30 = new LogicGates.AndGate();
         var endGate = new LogicGates.LoginGate();
-        
-        _switches.Add(new LogicGates.Switch());
-        _switches.Add(new LogicGates.Switch());
-        _switches.Add(new LogicGates.Switch());
-        _switches.Add(new LogicGates.Switch());
-        _switches.Add(new LogicGates.Switch());
-        _switches.Add(new LogicGates.Switch());
-        
-        LogicGates.ConnectComponents(_switches[0], and10, 0);
-        LogicGates.ConnectComponents(_switches[1], and10, 1);
-        LogicGates.ConnectComponents(_switches[2], and11, 0);
-        LogicGates.ConnectComponents(_switches[3], and11, 1);
-        LogicGates.ConnectComponents(_switches[4], or12, 0);
-        LogicGates.ConnectComponents(_switches[5], or12, 1);
-        
-        LogicGates.ConnectComponents(and10, and20, 0);
-        LogicGates.ConnectComponents(and11, and20, 1);
-        LogicGates.ConnectComponents(and11, or21, 0);
-        LogicGates.ConnectComponents(or12, or21, 1);
-        
-        LogicGates.ConnectComponents(and20, and30, 0);
-        LogicGates.ConnectComponents(or21, and30, 1);
 
-        LogicGates.ConnectComponents(and30, endGate, 0);
+        _switches.Add(new LogicGates.Switch());
+        _switches.Add(new LogicGates.Switch());
+        _switches.Add(new LogicGates.Switch());
+        _switches.Add(new LogicGates.Switch());
+        _switches.Add(new LogicGates.Switch());
+        _switches.Add(new LogicGates.Switch());
 
-        _switches[0].Toggle();
-        _switches[1].Toggle();
-        _switches[2].Toggle();
-        _switches[3].Toggle();
-        _switches[4].Toggle();
-        _switches[5].Toggle();
+        LogicGates.ConnectComponents(_switches[0], and10, 0, _puzzleLayer, new List<GridPosition>(){}, new List<GridPosition>(){new GridPosition(10, 1), new GridPosition(11,1)});
+        LogicGates.ConnectComponents(_switches[1], and10, 1, _puzzleLayer, new List<GridPosition>(){}, new List<GridPosition>(){new GridPosition(10, 9), new GridPosition(11,9)});
+        LogicGates.ConnectComponents(_switches[2], and11, 0, _puzzleLayer, new List<GridPosition>(){}, new List<GridPosition>(){new GridPosition(10, 17), new GridPosition(11,17)});
+        LogicGates.ConnectComponents(_switches[3], and11, 1, _puzzleLayer, new List<GridPosition>(){}, new List<GridPosition>(){new GridPosition(10, 25), new GridPosition(11,25)});
+        LogicGates.ConnectComponents(_switches[4], or12, 0, _puzzleLayer, new List<GridPosition>(){}, new List<GridPosition>(){new GridPosition(10, 33), new GridPosition(11,33)});
+        LogicGates.ConnectComponents(_switches[5], or12, 1, _puzzleLayer, new List<GridPosition>(){}, new List<GridPosition>(){new GridPosition(10, 41), new GridPosition(11,41)});
+
+        LogicGates.ConnectComponents(and10, and20, 0, _puzzleLayer, new List<GridPosition>(){new GridPosition(7, 6), new GridPosition(7, 7), new GridPosition(7, 8)}, new List<GridPosition>(){new GridPosition(8, 5), new GridPosition(7, 9)});
+        LogicGates.ConnectComponents(and11, and20, 1, _puzzleLayer, new List<GridPosition>(){new GridPosition(7, 18), new GridPosition(7, 19), new GridPosition(7, 20)}, new List<GridPosition>(){new GridPosition(8, 21), new GridPosition(7, 17)});
+        LogicGates.ConnectComponents(and11, or21, 0, _puzzleLayer, new List<GridPosition>(){new GridPosition(7, 22), new GridPosition(7, 23), new GridPosition(7, 24)}, new List<GridPosition>(){new GridPosition(8, 21), new GridPosition(7, 25)});
+        LogicGates.ConnectComponents(or12, or21, 1, _puzzleLayer, new List<GridPosition>(){new GridPosition(7, 34), new GridPosition(7, 35), new GridPosition(7, 36)}, new List<GridPosition>(){new GridPosition(8, 37), new GridPosition(7, 33)});
+
+        LogicGates.ConnectComponents(and20, and30, 0, _puzzleLayer, new List<GridPosition>(){new GridPosition(4, 14), new GridPosition(4, 15), new GridPosition(4, 16)}, new List<GridPosition>(){new GridPosition(5, 13), new GridPosition(4, 17)});
+        LogicGates.ConnectComponents(or21, and30, 1, _puzzleLayer, new List<GridPosition>(){new GridPosition(4, 26), new GridPosition(4, 27), new GridPosition(4, 28)}, new List<GridPosition>(){new GridPosition(4, 25), new GridPosition(5, 29)});
+
+        LogicGates.ConnectComponents(and30, endGate, 0, _puzzleLayer, new List<GridPosition>(){}, new List<GridPosition>(){new GridPosition(2, 21), new GridPosition(1, 21)});
     }
-    
+
+    private void LoadLogicPuzzleToLayer()
+    {
+        _puzzleLayer.WriteText(
+@"                 [ Login ]
+                     .
+                     .
+                 [  And  ]
+             .....       .....
+             .               .
+         [  And  ]       [  OR   ]
+     .....       .........       .....
+     .               .               .
+ [  And  ]       [  And  ]       [  OR   ]
+ .       .       .       .       .       .
+ .       .       .       .       .       .
+[ ]     [ ]     [ ]     [ ]     [ ]     [ ]");
+    }
+
+    private void SelectSwitch(List<KeyCode> args)
+    {
+        Debug.Log(_selectedSwitch);
+        _switches[_selectedSwitch].Toggle();
+    }
+
+    private void MoveSelectLeft(List<KeyCode> args)
+    {
+        print("Left called");
+        _selectedSwitch -= 1;
+        if (_selectedSwitch < 0) _selectedSwitch = 0;
+    }
+
+    private void MoveSelectRight(List<KeyCode> args)
+    {
+        print("Right called");
+        _selectedSwitch += 1;
+        if (_selectedSwitch >= _switches.Count) _selectedSwitch = _switches.Count - 1;
+    }
+
+
     // private void WriteText(string monitorText)
     // {
     //     _puzzleLayer.WriteText(monitorText);
