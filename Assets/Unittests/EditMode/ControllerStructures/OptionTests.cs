6610abde2845153c70510e5475b5ffe6250b9fc8
Bas van Rossem <bas.v.rossem@gmail.com>
2020-11-03 16:40:50 +0100

    Added frame for options tests

---
+++ Assets/Unittests/EditMode/ControllerStructures/OptionTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace ControllerStructures
+{
+    public class OptionTests
+    {
+        [Test]
+        public void OptionTestsSimplePasses()
+        {
+            // Use the Assert class to test conditions.
+            
+        }
+    }
+}
